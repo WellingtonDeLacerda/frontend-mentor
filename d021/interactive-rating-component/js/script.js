725130a3c58040e1b879aa75b2fbbb20345fde73
WellingtonDeLacerda <Wellingtonlacerda1515@gmail.com>
2026-04-02 13:10:57 -0300

    desafio 21 feito

---
+++ d021/interactive-rating-component/js/script.js
@@ -0,0 +1,43 @@
+const form = document.querySelector("form")
+const input = document.querySelectorAll("form input")
+const section = document.querySelectorAll("section")
+const label = document.querySelectorAll(".form-label")
+const res = document.querySelector("span.res")
+
+form.addEventListener("submit", (evt) => {
+    evt.preventDefault()
+    let isValid = false
+    input.forEach((e,i) => {
+       if (e.checked) {
+           isValid = true
+       }else {
+           isValid = false
+       }
+       if (!isValid) {
+       // nada acontece se n selecionar o botão
+       }else {
+           section[0].classList.remove("section-active")
+           section[0].classList.add("section-disabled")
+           section[1].classList.add("section-active")
+           section[1].classList.remove("section-disabled")
+           res.innerText = e.value
+       }
+    })
+})
+//label ativado
+input.forEach((e,i) => {
+    if (e.checked) {
+        label[i].classList.remove("label-normal")
+        label[i].classList.add("label-active")
+    }
+    e.addEventListener("change", () => {
+        label.forEach(index => {
+        index.classList.add("label-normal")
+        index.classList.remove("label-active")
+        })
+        if (e.checked) {
+        label[i].classList.remove("label-normal")
+        label[i].classList.add("label-active")
+        }        
+    })
+})
