3d7ce298e4bbfff327ce273862fc8c92c20754cb
WellingtonDeLacerda <Wellingtonlacerda1515@gmail.com>
2026-04-04 11:56:18 -0300

    update no desafio 18

--- d018/base-apparel-coming-soon-master/js/script.js
+++ d018/base-apparel-coming-soon-master/js/script.js
@@ -1,25 +1,45 @@
-const btn = document.querySelector(".btn")
 const email = document.querySelector("#mail")
 const res = document.querySelector(".res")
-const erro = document.querySelector("#icon-error")
+const erro = document.querySelector(".icon")
+const form = document.querySelector("form")
+const campo = document.querySelector(".campo_email")
 
-btn.addEventListener("click", () => {
-    if (email.value.length === 0) {
-        // caixa vazia
-        res.innerHTML = "Fill in the data"
-    }else {
-        if (email.value.includes("@email.com") || email.value.includes("@gmail.com")) {
-            // have a @
-            res.innerHTML = ""
-            erro.classList.remove("error")
-            erro.classList.add("semError")
-            email.style.border =
-             "2px solid var(--pink400)"
-        }else {
-            res.innerHTML = "Please provid a valid email"
-            erro.classList.remove("semError")
-            erro.classList.add("error")
-            email.style.border = "2px solid var(--red500)"
-        }
+form.addEventListener("submit", (evt) => {
+    evt.preventDefault()
+    let isValid = true
+    const regexEmail = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    
+    if (email.value.length === 0 || !regexEmail.test(email.value)) {
+        isValid = false
+        email.value = ""
+        email.placeholder = "Please provid a valid email"
     }
+    if (!isValid) {
+        res.innerHTML = "Fill in the data"
+        erro.classList.remove("icon-error-off")
+        erro.classList.add("icon-error-on")
+        campo.classList.add("campo_error")
+        campo.classList.remove("campo_normal")
+        email.classList.remove("placeholder_normal")
+        email.classList.add("placeholder_error")
+    }else {
+        res.innerHTML = ""
+        erro.classList.add("icon-error-off")
+        erro.classList.remove("icon-error-on")
+        email.placeholder = "Email address"
+        campo.classList.remove("campo_error")
+        campo.classList.add("campo_normal")
+        email.classList.add("placeholder_normal")
+        email.classList.remove("placeholder_error")
+    
+    }
+})
+//foco no campo de email
+email.addEventListener("focus", () => {
+    campo.classList.add("campo_focus")
+    campo.classList.remove("campo_normal")
+})
+email.addEventListener("blur", () => {
+    campo.classList.remove("campo_focus")
+    campo.classList.add("campo_normal")
 })
