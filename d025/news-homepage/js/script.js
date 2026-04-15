d187dc6e69dad1d5d17467945654ea15225b0c76
WellingtonDeLacerda <Wellingtonlacerda1515@gmail.com>
2026-04-15 10:22:19 -0300

    melhorado

--- d025/news-homepage/js/script.js
+++ d025/news-homepage/js/script.js
@@ -3,6 +3,7 @@ const nav = document.querySelector(".navbar")
 const iconMenu = document.querySelector(".icon_menu")
 const html = document.querySelector("html")
 const main = document.querySelector("main")
+const back = document.querySelector(".back")
 
 menu.addEventListener("click", () => {
     if (nav.classList.contains("nav-off")) {
@@ -11,11 +12,15 @@ menu.addEventListener("click", () => {
         iconMenu.src = "images/icon-menu-close.svg"
         html.classList.add("ligado")
         html.classList.remove("desligado")
+        back.classList.add("fundo-on")
+        back.classList.remove("fundo-off")
     }else {
         nav.classList.add("nav-off")
         nav.classList.remove("nav-on")
         iconMenu.src = "images/icon-menu.svg"
         html.classList.remove("ligado")
         html.classList.add("desligado")
+        back.classList.remove("fundo-on")
+        back.classList.add("fundo-off")
     }
 })
