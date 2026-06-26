f415da1183174ecf0dbcdb3aa1374c877a5dd8ed
WellingtonDeLacerda <Wellingtonlacerda1515@gmail.com>
2026-06-26 09:40:02 -0300

    codigo melhorado

--- d029/contact-form/assets/js/script.js
+++ d029/contact-form/assets/js/script.js
@@ -1,160 +1,138 @@
-const form = document.querySelector("form")
-const erroBox = form.querySelectorAll(".erro-box")
-const erroInput = form.querySelectorAll("input")
-const firstBoxes = form.querySelectorAll(".first-boxes")
-const firstTxts = form.querySelectorAll(".first-txts")
-const queryRadios = form.querySelectorAll(".box-radios input")
-const inputRadios =  form.querySelectorAll(".input-box input")
-const labelRadios = form.querySelectorAll(".box-radios .input-radio")
-const boxRadios = form.querySelector(".query .erro-off")
-const boxFocus = form.querySelectorAll(".query .input-box")
-const textArea = form.querySelector("textarea")
-const message = form.querySelector(".box-message .erro-off")
-const checkBox = form.querySelector(".custom-checkbox .erro-off")
-const inputCheck = form.querySelector(".custom-checkbox input")
-const labelCheck = form.querySelector(".custom-checkbox .input-checkbox")
-const aviseSuccess = form.querySelector(".avise-success")
+const erro = {
+    required: "This field is required",
+    email: "Please enter a valid email address",
+    queryRadio: "Please select a query type",
+    checkbox: "To submit this form, please consent to being contacted"
+}
+const {
+    required,
+    email,
+    queryRadio,
+    checkbox
+} = erro 
 
-form.addEventListener("submit", (event) => {
-    event.preventDefault()
-    
-    let isActive = true 
-    let index = 0
-    for(let i = 0; i < erroBox.length; i++) {
-        
-        if (erroInput[i].type === "text" || erroInput[i].type === "email") {
-            
-            if (erroInput[i].value.trim() === "") {
-                console.log(erroInput[i].name + " vazio")
-                firstTxts[i].classList.add("erro-on")
-                firstTxts[i].classList.remove("erro-off")
-                firstBoxes[index].classList.add("erro-active")
-                firstBoxes[index].classList.remove("erro-deactived")
-                firstTxts[i].innerText = "This field is required"
-                isActive = false
-            }else {
-                  firstTxts[i].classList.add("erro-off")
-                firstTxts[i].classList.remove("erro-on")
-                firstBoxes[index].classList.remove("erro-active")
-                firstBoxes[index].classList.add("erro-deactived")
-                
-                 if (erroInput[i].type === "email") {
-                     const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/
-                     
-                     if (!regex.test(erroInput[i].value)) {
-                         console.log("Email ivalido")
-                         firstTxts[i].classList.add("erro-on")
-                firstTxts[i].classList.remove("erro-off")
-                firstBoxes[index].classList.add("erro-active")
-                firstBoxes[index].classList.remove("erro-deactived")
-                         firstTxts[i].innerText = "Please enter a valid email address"
-                         isActive = false
-                     }
-                 }
-            }
-        }
-        /* continuar com outros inputs */
-        if (erroInput[i].type === "radio") {
-            if (!queryRadios[0].checked && !queryRadios[1].checked) {
-                console.log(inputRadios[index].value + " vazio")
-                boxRadios.classList.add("erro-on")
-                boxRadios.classList.remove("erro-off")
-                isActive = false
-            }else {
-                boxRadios.classList.remove("erro-on")
-                boxRadios.classList.add("erro-off")
-                // aqui vou focar em criar a lógica do input radio
-                if (inputRadios[index].checked) {
-                    console.log(inputRadios[index].value)
-                    
-                }
-            }
-        }
-        if (textArea && index == 4) {
-            if (textArea.value == "") {
-                console.log("Escreva uma mensagem")
-                textArea.classList.remove("erro-deactived")
-                textArea.classList.add("erro-active")
-                message.classList.remove("erro-off")
-                message.classList.add("erro-on")
-                isActive = false
-            }else {
-                textArea.classList.add("erro-deactived")
-                textArea.classList.remove("erro-active")
-                message.classList.remove("erro-on")
-                message.classList.add("erro-off")
-            }
-        }
-        if (erroInput[i].type === "checkbox") {
-            if (!erroInput[i].checked) {
-                console.log("Concorde com nossos termos")
-                checkBox.classList.add("erro-on")
-                checkBox.classList.remove("erro-off")
-                isActive = false
-            }else {
-                checkBox.classList.remove("erro-on")
-                checkBox.classList.add("erro-off")
-            }
-        }
-        index++
-    }  
-    if (!isActive) return
-    else {
-        console.log("tudo ok")
-        setTimeout(() => {
-            aviseSuccess.classList.add("avise-off")
-            aviseSuccess.classList.remove("avise-on")
-        },2000)
-        aviseSuccess.classList.add("avise-on")
-            aviseSuccess.classList.remove("avise-off")
-        form.reset()
-        labelRadios.forEach((label,bf) => {
-            label.classList.add("radio-off")
-            label.classList.remove("radio-on")
-            boxFocus[bf].classList.remove("radio-focus")
-        })
-        labelCheck.classList.add("checkbox-off")
-        labelCheck.classList.remove("checkbox-on")
-    }
-})
-queryRadios.forEach((radio,irad) => {
-    radio.addEventListener("change", () => {
-        labelRadios.forEach((label,bf) => {
-            label.classList.add("radio-off")
-            label.classList.remove("radio-on")
-            boxFocus[bf].classList.remove("radio-focus")
-        })
-        if (radio.checked) {
-            labelRadios[irad].classList.add("radio-on")
-        labelRadios[irad].classList.remove("radio-off")
-        boxFocus[irad].classList.add("radio-focus")
-        }
-    })
-    
-})
-inputCheck.addEventListener("change", () => {
-    if (inputCheck.checked) {
-        labelCheck.classList.add("checkbox-on")
-        labelCheck.classList.remove("checkbox-off")
-    }else {
-        labelCheck.classList.add("checkbox-off")
-        labelCheck.classList.remove("checkbox-on")
-    }
-})
-firstBoxes.forEach((focus,i) => {
-    const input = focus.querySelector("input")
+const form = document.querySelector(".register")
+const boxFocus = form.querySelectorAll(".box-normal")
+const boxLabel = form.querySelectorAll(".box-label")
+
+boxFocus.forEach(value => {
+    const input = value.querySelector("input")
     input.addEventListener("focus", () => {
-        focus.classList.remove("erro-deactived")
-        focus.classList.add("focus-box")
-        if (focus.classList.contains('erro-active')) {
-            focus.classList.remove("focus-box")
+        if (!value.classList.contains("box-erro")) {
+            value.classList.add("box-focus")
+            value.classList.remove("box-normal")
         }
     })
     input.addEventListener("blur", () => {
-        focus.classList.remove("focus-box")
-        focus.classList.add("erro-deactived")
-        if (focus.classList.contains('erro-active')) {
-            focus.classList.remove("erro-deactived")
+        if (!value.classList.contains("box-erro")) {
+            value.classList.remove("box-focus")
+            value.classList.add("box-normal")
         }
     })
+})
+let checkRadio
+const queryInput = form.querySelectorAll(".query-radio input")
+const queryRadios = form.querySelectorAll(".query-box .input-box") 
+const circle = form.querySelectorAll(".radio-circle")
+queryInput.forEach((input,i) => {
+    input.addEventListener("change", () => {
+       queryRadios.forEach((radio,i) => {
+           radio.classList.remove("radio-focus")
+           
+           circle[i].classList.remove("radio-active")
+       })
+       if (input.checked) {
+           queryRadios[i].classList.add("radio-focus")
+           circle[i].classList.add("radio-active")
+           checkRadio = input.value 
+       } 
+    })
+})
+
+const checkSquare = form.querySelector(".check-flex .check-square")
+const customCheckbox = form.querySelector(".custom-checkbox")
+
+customCheckbox.addEventListener("input", () => {
+    if (customCheckbox.checked) {
+        checkSquare.classList.add("checkbox-active")
+    }else {
+    checkSquare.classList.remove("checkbox-active")
+    }
+})
+const aviseErro = form.querySelectorAll(".avise-erro")
+const inputs = form.querySelectorAll(".input")
+
+const addBoxErro = value => {
+    value.classList.remove("box-normal")
+    value.classList.add("box-erro")
+}
+const removeBoxErro = value => {
+    value.classList.add("box-normal")
+    value.classList.remove("box-erro")
+}
+const textArea = form.querySelector("textarea.message")
+const rollScroll = () => {
+    window.scrollTo({
+        top:0,
+        behavior: 'smooth'
+    })
+}
+form.addEventListener("submit", (event) => {
+    event.preventDefault()
+    let isActive = true
+    let index = 0
+    for (const items of inputs) {
+        if (items.value.trim() === "") {
+            aviseErro[index].innerText = required
+            isActive = false
+            addBoxErro(boxFocus[index])
+        }else {
+            removeBoxErro(boxFocus[index])
+            aviseErro[index].innerText = ""
+            if (items.name === "email") {
+                const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/
+                if (!regex.test(items.value)) {
+                    addBoxErro(boxFocus[index])
+                    isActive = false
+                    aviseErro[index].innerText = email
+                }
+            }
+        }
+        index++
+    }
+    if (queryInput[0].checked || queryInput[1].checked) {
+        aviseErro[index].innerText = ""
+    }else {
+        aviseErro[index].innerText = queryRadio
+        isActive = false
+    }
+    
+    if (textArea.value === "") {
+        aviseErro[index + 1].innerText = required
+        textArea.classList.add("box-erro")
+        isActive = false
+    }else {
+        aviseErro[index + 1].innerText = ""
+        textArea.classList.remove("box-erro")
+    }
+    
+    if (customCheckbox.checked) {
+        aviseErro[index + 2].innerText = ""
+    }else {
+        aviseErro[index + 2].innerText = checkbox
+        isActive = false
+    }
+    
+    if (!isActive) return
+    else {
+        // tudo ok
+        rollScroll()
+        const checkMsg = document.querySelector(".message-sent")
+        checkMsg.classList.add("check-on")
+        checkMsg.classList.remove("check-off")
+        setTimeout(() => {
+            checkMsg.classList.add("check-off")
+            checkMsg.classList.remove("check-on")
+        },3000)
+    }
 })
